9ebdbcd86f2ef0763803b7a08fab39302d16a883
MinesSkyline <dhaval-vashi@hotmail.com>
2014-02-28 19:51:43 -0500

    EncoderUnitTest Updated
    Tested new encoders. Ticks are still skipping but it works otherwise.

--- Unit Tests/EncoderUnitTest/EncoderUnitTest.ino
+++ Unit Tests/EncoderUnitTest/EncoderUnitTest.ino
@@ -1,16 +1,12 @@
 #include <Encoder.h>
 #include <Motor.h>
 #include <RobotController.h>
-#include <PID.h>
 
 Motor left_motor(8, 10, false);    // Left Motor, Dir pin 8, PWM pin 10, direction revered
 Motor right_motor(7, 9, true);   // Right Motor, Dir pin 7, PWM pin 9, direction not reversed
 
-Encoder left_encoder(2, 4, &left_encoder_distance, &left_encoder_velocity, false);
-Encoder right_encoder(3, 5, &right_encoder_distance, &right_encoder_velocity, true);
-
-PID left_PID(&left_encoder_velocity, &left_motor_PWM, &left_motor_setpoint, KP, KI, KD, DIRECT);
-PID right_PID(&right_encoder_velocity, &right_motor_PWM, &right_motor_setpoint, KP, KI, KD, DIRECT);
+Encoder left_encoder(2, 4, &left_encoder_distance, &left_encoder_velocity, true);
+Encoder right_encoder(3, 5, &right_encoder_distance, &right_encoder_velocity, false);
 
 void setup() 
 {
@@ -19,37 +15,52 @@ void setup()
   right_encoder.setHighPinA();
   right_encoder.setHighPinB();
   
-  left_PID.SetMode(AUTOMATIC);
-  left_PID.SetOutputLimits(-255, 255);
-  right_PID.SetMode(AUTOMATIC);
-  right_PID.SetOutputLimits(-255, 255);  
-  
   attachInterrupt(0, updateLeftEncoder, CHANGE);
   attachInterrupt(1, updateRightEncoder, CHANGE);
   
   Serial.begin(9600);
+  
   pinMode(12,INPUT);
   digitalWrite(12,HIGH);
   while(digitalRead(12)){}
-   left_motor_setpoint=1000;
 }
 
 void loop()
 {
-  //left_motor.setSpeed(255);
+  if (!digitalRead(12))
+  {
+    left_encoder_distance = 0;
+    right_encoder_distance = 0;
+  }
 
-   Serial.print(left_encoder_velocity);
-  
-  Serial.print(",");
- Serial.print(left_encoder_distance);
-  Serial.println(",0");
-  delay (10);
-  right_encoder.compute();
   left_encoder.compute();
-  right_PID.Compute();
-  left_PID.Compute();
-  //if(left_encoder_distance >= 2980) {left_motor.setSpeed(0); delay(500); left_motor.setSpeed(-100);}
-  //if(left_encoder_distance <0) {left_motor.setSpeed(0); delay(500);  left_motor.setSpeed(100);}
+  right_encoder.compute();
+  
+  if (left_encoder_distance < 894 - 400) left_motor_PWM = 180;
+  else if (left_encoder_distance < 894 - 200) left_motor_PWM = 120;
+  else if (left_encoder_distance < 894 - 110) left_motor_PWM = 20;
+  else left_motor_PWM = 0;
+  
+  if (right_encoder_distance < 894 - 400) right_motor_PWM = 180;
+  else if(right_encoder_distance < 894 - 200) right_motor_PWM = 120;
+  else if(right_encoder_distance < 894 - 110) right_motor_PWM = 20;
+  else right_motor_PWM = 0;
+  
+  setMotors();  
+  
+  Serial.print(left_encoder_distance);
+  Serial.print(" ");
+  Serial.print(left_encoder_velocity);
+  Serial.print(" ");
+  Serial.print(right_encoder_distance);
+  Serial.print(" ");
+  Serial.println(right_encoder_velocity);  
+}
+
+void setMotors()
+{
+  left_motor.setSpeed(left_motor_PWM);
+  right_motor.setSpeed(right_motor_PWM); 
 }
 
 void updateLeftEncoder()
