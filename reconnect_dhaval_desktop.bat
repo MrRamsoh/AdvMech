f95987139166b698ef55cfb4e3da2611cd06032d
MinesSkyline <dhaval-vashi@hotmail.com>
2014-03-03 18:37:32 -0500

    Added new .bat file for Dhaval Desktop

---
+++ reconnect_dhaval_desktop.bat
@@ -0,0 +1,19 @@
+rd /s /q E:\Dropbox\Arduino\libraries\Encoder
+rd /s /q E:\Dropbox\Arduino\libraries\IRSensor
+rd /s /q E:\Dropbox\Arduino\libraries\LightSensor
+rd /s /q E:\Dropbox\Arduino\libraries\Motor
+rd /s /q E:\Dropbox\Arduino\libraries\PID
+rd /s /q E:\Dropbox\Arduino\libraries\RobotController
+rd /s /q E:\Dropbox\Arduino\libraries\TaskManager
+
+PAUSE
+
+mklink /j E:\Dropbox\Arduino\libraries\Encoder C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\Encoder
+mklink /j E:\Dropbox\Arduino\libraries\IRSensor C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\IRSensor
+mklink /j E:\Dropbox\Arduino\libraries\LightSensor C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\LightSensor
+mklink /j E:\Dropbox\Arduino\libraries\Motor C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\Motor
+mklink /j E:\Dropbox\Arduino\libraries\PID C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\PID
+mklink /j E:\Dropbox\Arduino\libraries\RobotController C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\RobotController
+mklink /j E:\Dropbox\Arduino\libraries\TaskManager C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\TaskManager
+
+PAUSE
