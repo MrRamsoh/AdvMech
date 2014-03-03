c8046b866be379613b0e1242e9e96c4ecb4a4b5b
MinesSkyline <dhaval-vashi@hotmail.com>
2014-03-03 15:33:09 -0500

    Added new .bat file for Dhaval

---
+++ reconnect_dhaval.bat
@@ -0,0 +1,19 @@
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\Encoder
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\IRSensor
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\LightSensor
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\Motor
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\PID
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\RobotController
+rd /s /q C:\Users\100396659\Dropbox\Arduino\libraries\TaskManager
+
+PAUSE
+
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\Encoder C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\Encoder
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\IRSensor C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\IRSensor
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\LightSensor C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\LightSensor
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\Motor C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\Motor
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\PID C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\PID
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\RobotController C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\RobotController
+mklink /j C:\Users\100396659\Dropbox\Arduino\libraries\TaskManager C:\Users\100396659\Documents\GitHub\AdvMech\Libraries\TaskManager
+
+PAUSE
