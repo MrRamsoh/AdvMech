4e71bf3935979338a110c60e4bcf50f7142cd2f1
Mike Hosmar <mikehosmar@gmail.com>
2014-03-04 23:19:24 -0500

    desktop_reconnect

---
+++ reconnect_desktop.bat
@@ -0,0 +1,21 @@
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\Encoder
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\IRSensor
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\LightSensor
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\Motor
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\PID
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\RobotController
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\TaskManager
+rd /s /q C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\Gyro
+
+PAUSE
+
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\Encoder C:\Users\michael\Documents\GitHub\AdvMech\Libraries\Encoder
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\IRSensor C:\Users\michael\Documents\GitHub\AdvMech\Libraries\IRSensor
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\LightSensor C:\Users\michael\Documents\GitHub\AdvMech\Libraries\LightSensor
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\Motor C:\Users\michael\Documents\GitHub\AdvMech\Libraries\Motor
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\PID C:\Users\michael\Documents\GitHub\AdvMech\Libraries\PID
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\RobotController C:\Users\michael\Documents\GitHub\AdvMech\Libraries\RobotController
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\TaskManager C:\Users\michael\Documents\GitHub\AdvMech\Libraries\TaskManager
+mklink /j C:\Users\michael\Dropbox\projects\arduino-1.0.5\libraries\Gyro C:\Users\michael\Documents\GitHub\AdvMech\Libraries\Gyro
+
+PAUSE
