5a236ed387a1309b6200d900e98017c1a2c43c44
MinesSkyline <dhaval-vashi@hotmail.com>
2014-03-13 15:05:06 -0400

    Updated reconnect.bat

--- reconnect_dhaval_desktop.bat
+++ reconnect_dhaval_desktop.bat
@@ -5,6 +5,8 @@ rd /s /q E:\Dropbox\Arduino\libraries\Motor
 rd /s /q E:\Dropbox\Arduino\libraries\PID
 rd /s /q E:\Dropbox\Arduino\libraries\RobotController
 rd /s /q E:\Dropbox\Arduino\libraries\TaskManager
+rd /s /q E:\Dropbox\Arduino\libraries\Gyro
+rd /s /q E:\Dropbox\Arduino\libraries\Segment
 
 PAUSE
 
@@ -15,5 +17,7 @@ mklink /j E:\Dropbox\Arduino\libraries\Motor C:\Users\dhava_000\Documents\GitHub
 mklink /j E:\Dropbox\Arduino\libraries\PID C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\PID
 mklink /j E:\Dropbox\Arduino\libraries\RobotController C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\RobotController
 mklink /j E:\Dropbox\Arduino\libraries\TaskManager C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\TaskManager
+mklink /j E:\Dropbox\Arduino\libraries\Gyro C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\Gyro
+mklink /j E:\Dropbox\Arduino\libraries\Segment C:\Users\dhava_000\Documents\GitHub\AdvMech\Libraries\Segment
 
 PAUSE
